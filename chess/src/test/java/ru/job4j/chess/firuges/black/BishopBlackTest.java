7188fa33c7e4b6f79d51950322223522874ac298
Ilya96s <i.kaltygin@icloud.com>
2022-01-13 20:59:20 +0300

    Каркас шахматной доски

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import javax.crypto.spec.RC5ParameterSpec;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+public class BishopBlackTest {
+
+    @Test
+    public void testPosition() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+        bishopBlack.position();
+        assertThat(bishopBlack.position(), is(Cell.A1));
+    }
+
+    @Test
+    public void testCopy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+        Figure returned = bishopBlack.copy(Cell.A1);
+        assertThat(returned.position(), is(bishopBlack.position()));
+    }
+
+    @Test
+    public void testWay() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] rsl = bishopBlack.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertArrayEquals(expected, rsl);
+    }
+
+    @Test
+    public void testIsDiagonal() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.G5);
+    }
+}
